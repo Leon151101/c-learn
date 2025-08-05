2b9d24eaa51ee4d5979935da464ee24e8184c882
Leon <123963348+Leon151101@users.noreply.github.com>
2025-08-05 23:58:43 +0200

    feat: add arithmetic operators chapter

---
+++ 04_arithmetic_operators/arithmetic_operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    int y = 3;
+    double z;
+   
+    z = x + y;
+    //z = x - y;
+    //z = x * y;
+    //z = x / y;
+    //z = x / (double)y;
+    //z = x % y;
+    //x++;
+    //x--;
+    //x += 2;
+    //x -= 2;
+    //x *= 2;
+    //x /= 2;
+    //x %= 2;
+
+
+    printf("Result: %.2lf", z);
+
+    return 0;
+}
