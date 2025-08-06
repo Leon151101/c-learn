c80709d426bbcc1a241af9b0fd80e6d677e9de67
Leon <123963348+Leon151101@users.noreply.github.com>
2025-08-06 16:28:03 +0200

    feat: add math functions chapter

---
+++ 08_math_functions/math_functions.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = 3.14;
+    x = sqrt(x);
+    //x = pow(x, 2);
+    //x = round(x);
+    //x = ceil(x);
+    //x = floor(x);
+    //x = abs(x); 
+    //x = log(x); // ln 
+    //x = log10(x);
+    //x = sin(x);
+    //x = cos(x);
+    //x = tan(x);
+
+    printf("%lf", x);
+
+    return 0;
+}
